3212e9d18cc0dcfeea0b06aaa093297efd6875c9
Noah <noah@n0ahmc.com>
2021-02-20 00:10:52 -0600

    Added guildMemberRemove event messages

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const mongoose = require('mongoose');
+const Guild = require('../models/guild');
+
+module.exports = async (client, member) => {
+    const settings = await Guild.findOne({
+        guildID: member.guild.id
+    }, (err, guild) => {
+        if (err) console.error(err)
+    });
+    if (!settings.welcomeChannelID) {return true}
+    member.guild.channels.cache.get(settings.welcomeChannelID).send(new Discord.MessageEmbed()
+        .setTitle(`Goodbye ${member.user.tag}!`)
+        .setDescription(`${member.user.tag} has left ${member.guild.name}!\n**Current Members:** ${member.guild.memberCount}`)
+        .setTimestamp()
+        .setColor(process.env.EMBED_COLOR)
+        .setFooter(process.env.EMBED_FOOTER, process.env.EMBED_FOOTER_IMAGE)
+        )
+};
