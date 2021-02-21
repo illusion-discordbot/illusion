596e16d0f3a81bfcc25b8fe78ac4314a996c837a
Noah <noah@n0ahmc.com>
2021-02-21 01:04:26 -0600

    Added uptime command

---
+++ commands/utility/uptime.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const moment = require('moment')
+require('moment-duration-format')
+module.exports = {
+	name: 'uptime',
+	description: 'Shows uptime of bot!',
+    category: 'utility',
+	cooldown: 5,
+	execute(client, message, args) {
+        const uptime = moment
+      .duration(client.uptime)
+      .format('D [days], H [hrs], m [mins], s [secs]')
+        message.channel.send(new Discord.MessageEmbed()
+        .setTitle(`:chart_with_upwards_trend:  |  I've been running for **${uptime}**!`)
+        .setColor(process.env.EMBED_COLOR)
+        .setFooter(process.env.EMBED_FOOTER, process.env.EMBED_FOOTER_IMAGE)
+        )
+	},
+};
