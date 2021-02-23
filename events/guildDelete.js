a4a30f2a87e097f78cb095234db1558ff5635af9
Noah <noah@n0ahmc.com>
2021-02-23 15:17:32 -0600

    removed const Discord

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Guild = require('../models/guild');
-const Discord = require('discord.js');
 module.exports = async (client, guild) => {
     if (!guild.available) return;
     client.logger.cmd(`[GUILD LEAVE] ${guild.name} (${guild.id}) removed the bot.`);
